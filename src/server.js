917afc07482222c4c754907093cddd610b1de2d9
charlyhs24 <charlysitumorang24@gmail.com>
2020-07-04 19:27:46 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
